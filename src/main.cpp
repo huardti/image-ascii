f03474444b3f612c655f9eeb9f7f24778238272b
huardti <titouan.huard@gmail.com>
2020-09-29 22:23:04 +0200

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+#include "bmp/bmp.hpp"
+
+int main(int argc, char* argv[]) {
+
+    if (argc != 2) {
+        std::cout << "nombre d'argument incorect" << std::endl;
+        return 0;
+    }
+
+    const char* file = argv[1];
+    bmp::Image I(file);
+    I.convert_black_and_white(bmp::LUMINOSITE);
+
+    I.write("luminosite.bmp");
+    
+    return 0;
+
+}
